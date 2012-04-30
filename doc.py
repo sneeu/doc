1a3d0e0a5a6e3c0c0ea05cc209b4aa810c87937e
sneeu <john@sneeu.com>
2012-04-30 23:58:52 +0100

    Reads and writes unicode. Removes small-caps. Print links are smaller, and lighter.

--- doc.py
+++ doc.py
@@ -1,3 +1,4 @@
+import codecs
 import os
 import sys
 
@@ -24,13 +25,12 @@ p,ul,ol,.address{padding:0 0 1.5em}
 ul,ol{padding-left:1.5em}
 pre{background:#ddd;border:1px solid #bbb;font:100%%/1.5 Monaco,monospace;margin:-2px -1px 1.5em;padding:1.5em}
 .address span{display:block}
-p .caps,li .caps{font-variant:small-caps;text-transform:lowercase}
 </style>
 <style type="text/css" media="print">
 *{font-size:1em;font-style:inherit;font-weight:inherit;margin:0;padding:0}
 body{background:#fff;color:#222;font:100%%/1.5 "Helvetica Neue",Arial,Helvetica,sans-serif;padding:3em}
 a{color:inherit;font-weight:inherit;text-decoration:none}
-a:not([href^=mailto]):after{content: " (" attr(href) ")"}
+a:not([href^=mailto]):after{color:#666;content:" (" attr(href) ")";font-size:0.9em}
 .nolink a:after{content:""}
 strong{font-weight:bold}
 em{font-style:italic}
@@ -42,7 +42,6 @@ p,ul,ol,.address{padding:0 0 1.5em}
 ul,ol{padding-left:1.5em}
 pre{background:#ddd;border:1px solid #bbb;font:100%%/1.5 Monaco,monospace;margin:-2px -1px 1.5em;padding:1.5em}
 .address span{display:block}
-p .caps,li .caps{font-variant:small-caps;text-transform:lowercase}
 </style>
 </head>
 <body>"""
@@ -52,7 +51,7 @@ FOOTER = """</body></html>"""
 if __name__ == '__main__':
     if (len(sys.argv) > 1):
         filename = sys.argv[1]
-        in_file = open(filename, 'r')
+        in_file = codecs.open(filename, encoding='utf-8')
         md = in_file.read()
         in_file.close()
 
@@ -60,7 +59,7 @@ if __name__ == '__main__':
         content = typogrify.typogrify(
             markdown.markdown(md, ['abbr', 'headerid']))
 
-        out_file = open(filename + '.html', 'w')
+        out_file = codecs.open(filename + '.html', encoding='utf-8', mode='w+')
         out_file.write((HEADER % title) + content + FOOTER)
         out_file.close()
     else:
